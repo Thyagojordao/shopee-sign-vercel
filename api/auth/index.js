cc8daf5de25af5c510d89cd0cbd8417fc02d69cb
Thyago Jordão <thyagojordao@hotmail.com>
2025-06-13 18:10:07 -0300

    Forçando rebuild final para criar endpoint /api/auth

--- api/auth/index.js
+++ api/auth/index.js
@@ -8,10 +8,9 @@ module.exports = (req, res) => {
   }
 
   const baseString = `${partner_id}${path}${timestamp}`;
-  const sign = crypto
-    .createHmac('sha256', Buffer.from(partner_key, 'hex'))
-    .update(baseString)
-    .digest('hex');
+  const sign = crypto.createHmac('sha256', Buffer.from(partner_key, 'hex'))
+                     .update(baseString)
+                     .digest('hex');
 
   res.status(200).json({ sign });
 };
