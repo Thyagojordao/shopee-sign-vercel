d01a3659cb342dadb9f4e47cd6354d42a7c87e6e
Thyago Jordão <thyagojordao@hotmail.com>
2025-06-13 19:41:18 -0300

    Corrigindo vercel.json e exportação final compatível com Vercel

--- api/auth/index.js
+++ api/auth/index.js
@@ -1,16 +1,21 @@
 const crypto = require('crypto');
 
-module.exports = (req, res) => {
-  const { partner_id, path, timestamp, partner_key } = req.query;
+module.exports = async (req, res) => {
+  try {
+    const { partner_id, path, timestamp, partner_key } = req.query;
 
-  if (!partner_id || !path || !timestamp || !partner_key) {
-    return res.status(400).json({ error: 'Missing parameters' });
+    if (!partner_id || !path || !timestamp || !partner_key) {
+      return res.status(400).json({ error: 'Missing parameters' });
+    }
+
+    const baseString = `${partner_id}${path}${timestamp}`;
+    const sign = crypto.createHmac('sha256', Buffer.from(partner_key, 'hex'))
+                       .update(baseString)
+                       .digest('hex');
+
+    res.status(200).json({ sign });
+  } catch (error) {
+    console.error('Server Error:', error);
+    res.status(500).json({ error: 'Internal Server Error' });
   }
-
-  const baseString = `${partner_id}${path}${timestamp}`;
-  const sign = crypto.createHmac('sha256', Buffer.from(partner_key, 'hex'))
-    .update(baseString)
-    .digest('hex');
-
-  res.status(200).json({ sign });
 };
