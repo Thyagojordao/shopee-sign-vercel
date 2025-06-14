827b4c6317e729c0817456a86df6fa07566823ff
Thyago Jordão <thyagojordao@hotmail.com>
2025-06-14 11:14:03 -0300

    Atualizando sign.js para correção final

--- api/sign.js
+++ api/sign.js
@@ -6,20 +6,19 @@ module.exports = (req, res) => {
     const partnerKey = process.env.PARTNER_KEY;
 
     if (!partner_id || !path || !timestamp || !partnerKey) {
-      return res.status(400).json({ error: 'Missing required parameters or partner key not set' });
+      return res.status(400).json({ error: 'Missing required parameters or partner key env not set.' });
     }
 
     const baseString = `${partner_id}${path}${timestamp}`;
-    console.log('🔑 Base string usada para geração do sign:', baseString);
 
     const sign = crypto
       .createHmac('sha256', partnerKey)
       .update(baseString)
       .digest('hex');
 
-    return res.status(200).json({ sign });
+    res.status(200).json({ sign });
   } catch (error) {
     console.error('Erro ao gerar sign:', error);
-    return res.status(500).json({ error: 'Erro interno na geração do sign', details: error.message });
+    res.status(500).json({ error: 'Erro interno na geração do sign', details: error.message });
   }
 };
